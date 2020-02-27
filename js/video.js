9fad00f042a0b91a67c7f696f4f49c3297e90464
Xuan <email.xhuang@gmail.com>
2020-02-27 14:29:10 -0500

    Initial commit

---
+++ js/video.js
@@ -0,0 +1,53 @@
+var video;
+
+function getVid(){
+	video = document.querySelector("#myVideo");
+}
+
+function playVid() {
+	video.play();
+	// console.log("Play Video");
+}
+
+function pauseVid() {
+	video.pause();
+	// console.log("Pause Video");
+}
+
+function decreaseSpeed() {
+
+  	console.log("Speed is " + '');
+}
+
+function increaseSpeed() {
+
+	console.log("Speed is "+ '');
+}
+
+function skipAhead() {
+
+	console.log("Current location is "+ '');
+}
+
+function mute() {
+
+	console.log("Unmuted");
+
+	console.log("Muted");
+}
+
+function changeVolume() {
+
+	console.log("Volume is ");
+}
+
+
+function gray() {
+
+	console.log("In grayscale")
+}
+
+function color() {
+
+	console.log("In color")
+}
